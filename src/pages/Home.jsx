7262ffeb39920ccfd7f9b906a89faa6b0ab49714
廖跃翔 <liaoyuexiang@ryous-MBP.lan>
2023-01-12 23:10:14 -0500

    commit to github

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import { useStateContext } from '../context';
+const Home = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home
